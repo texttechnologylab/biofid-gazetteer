ea59e1a2e2b330b1fed030e1ebd4519e80fd345e
Manuel Stoeckel <manuel.stoeckel@stud.uni-frankfurt.de>
2020-02-10 14:02:06 +0100

    Added test for regular gazetteer

--- src/test/java/org/biofid/gazetter/TestBIOfidGazetteer.java
+++ src/test/java/org/biofid/gazetter/TestBIOfidGazetteer.java
@@ -1,7 +1,9 @@
 package org.biofid.gazetter;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.impl.XmiCasSerializer;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.JCasFactory;
@@ -14,11 +16,40 @@ import org.texttechnologylab.annotation.type.Taxon;
 import org.xml.sax.SAXException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class TestBIOfidGazetteer {
+
+    @Test
+    public void testRegularGazetteer() {
+        try {
+            String sourceLocation = "src/test/resources/taxa.zip";
+//			String sourceLocation = "https://www.texttechnologylab.org/files/BIOfidTaxa.zip";
+            final AnalysisEngine gazetterEngine = AnalysisEngineFactory.createEngine(AnalysisEngineFactory.createEngineDescription(
+                    BIOfidGazetteer.class,
+                    BIOfidGazetteer.PARAM_SOURCE_LOCATION, sourceLocation,
+                    BIOfidGazetteer.PARAM_USE_LOWERCASE, false));
+
+            File file = new File("src/test/resources/9031034.xmi");
+            {
+                JCas jCas = JCasFactory.createJCas();
+                CasIOUtils.load(java.nio.file.Files.newInputStream(file.toPath()), null, jCas.getCas(), true);
+                jCas.removeAllIncludingSubtypes(Taxon.type);
+
+                run(gazetterEngine, jCas);
+
+                System.out.println(JCasUtil.select(jCas, Taxon.class).stream().map(taxon -> String.format("%s@(%d, %d): %s", taxon.getCoveredText(), taxon.getBegin(), taxon.getEnd(), taxon.getValue())).collect(Collectors.joining("\n")));
+            }
+        } catch (UIMAException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Test
     public void testCharGazetteer() {
@@ -28,7 +59,8 @@ public class TestBIOfidGazetteer {
             final AnalysisEngine gazetterEngine = AnalysisEngineFactory.createEngine(AnalysisEngineFactory.createEngineDescription(
                     BIOfidTreeGazetteer.class,
                     BIOfidTreeGazetteer.PARAM_SOURCE_LOCATION, sourceLocation,
-                    BIOfidTreeGazetteer.PARAM_USE_LOWERCASE, false));
+                    BIOfidTreeGazetteer.PARAM_USE_LOWERCASE, false,
+                    BIOfidTreeGazetteer.PARAM_USE_STRING_TREE, false));
 
             File file = new File("src/test/resources/9031034.xmi");
             {
@@ -36,8 +68,7 @@ public class TestBIOfidGazetteer {
                 CasIOUtils.load(java.nio.file.Files.newInputStream(file.toPath()), null, jCas.getCas(), true);
                 jCas.removeAllIncludingSubtypes(Taxon.type);
 
-                SimplePipeline.runPipeline(jCas, gazetterEngine);
-                XmiCasSerializer.serialize(jCas.getCas(), new FileOutputStream(new File("/tmp/temp.xmi")));
+                run(gazetterEngine, jCas);
 
                 System.out.println(JCasUtil.select(jCas, Taxon.class).stream().map(taxon -> String.format("%s@(%d, %d): %s", taxon.getCoveredText(), taxon.getBegin(), taxon.getEnd(), taxon.getValue())).collect(Collectors.joining("\n")));
             }
@@ -63,14 +94,20 @@ public class TestBIOfidGazetteer {
                 CasIOUtils.load(java.nio.file.Files.newInputStream(file.toPath()), null, jCas.getCas(), true);
                 jCas.removeAllIncludingSubtypes(Taxon.type);
 
-                SimplePipeline.runPipeline(jCas, gazetterEngine);
-                XmiCasSerializer.serialize(jCas.getCas(), new FileOutputStream(new File("/tmp/temp.xmi")));
+                run(gazetterEngine, jCas);
 
                 System.out.println(JCasUtil.select(jCas, Taxon.class).stream().map(taxon -> String.format("%s@(%d, %d): %s", taxon.getCoveredText(), taxon.getBegin(), taxon.getEnd(), taxon.getValue())).collect(Collectors.joining("\n")));
             }
         } catch (UIMAException | IOException | SAXException e) {
             e.printStackTrace();
         }
+    }
+
+    private void run(AnalysisEngine gazetterEngine, JCas jCas) throws AnalysisEngineProcessException, SAXException, FileNotFoundException {
+        StopWatch stopWatch = StopWatch.createStarted();
+        SimplePipeline.runPipeline(jCas, gazetterEngine);
+        XmiCasSerializer.serialize(jCas.getCas(), new FileOutputStream(new File("/tmp/temp.xmi")));
+        System.out.printf("Finished tagging in %dms.\n", stopWatch.getTime(TimeUnit.MILLISECONDS));
     }
 
 }
